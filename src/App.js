779b0eca1bfc597fa67681ecf7f7267a830111ea
eliotweber2 <eliotthomasweber2@gmail.com>
2024-07-02 19:51:47 -0400

    Added basic test lander

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+const lander = {};
+
+lander.nodeSize = 15;
+
+const screenParams = {};
+
+screenParams.width=500;
+screenParams.height=5000
+
+//lower leg length = 28.5
+const testLander = [
+  {x:200, y:200},
+  {x:200, y:160},
+  {x:171.5, y:228.5},
+  {x:228.5, y:228.5},
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const landerComp = genLanderComponent(testLander);
+  const screen = 
+    <svg width={screenParams.width} height={screenParams.height}>
+      {landerComp}
+    </svg>;
+  return screen;
+}
+
+function genLanderComponent(nodes) {
+  const nodeComponents = nodes.map(
+    (ele,ind) => <LanderNode key={ind} x={ele.x} y={ele.y} />
   );
+
+  return nodeComponents;
+}
+
+function LanderNode(props) {
+  return <circle className="node" cx={props.x} cy={props.y} r={lander.nodeSize/2} />;
 }
 
 export default App;

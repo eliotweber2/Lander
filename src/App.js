d1e6dec48aa6dbd0dd73167efdc1b02daa25294d
eliotweber2 <eliotthomasweber2@gmail.com>
2024-07-08 22:11:24 -0400

    Set up server connection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import SocketConnection from './landerServer/clientSocketInterface'
+import SocketConnection from './clientSocketInterface';
 
 const lander = {};
 
@@ -12,37 +12,26 @@ screenParams.height=5000;
 
 //lower leg length = 28.5
 
-// eslint-disable-next-line
-const testLander = [
-  {x:200, y:200},
-  {x:200, y:160},
-  {x:171.5, y:228.5},
-  {x:228.5, y:228.5},
-];
-
 
 function App() {
   function handleMessage(message) {
-    if (message.slice(0,4) === 'TEST') {setText(message.slice(5))}
+    if (message.slice(0,4) === 'LDRP') {setLanderPos(JSON.parse(message.slice(5)))}
   }
-  //const landerComp = genLanderComponent(landerPos);
   const socket = useRef(null);
-  const [text,setText] = useState('');
-  const test = <p>{text}</p>
+  const [landerPos, setLanderPos] = useState([]);
   useEffect(() => {
     const socketHandler = {};
     socketHandler.handleMessage = handleMessage;
     socket.current = new SocketConnection('ws://localhost:8080');
     socket.current.setHandler(socketHandler);
   }, [])
-  //const screen = 
-    //<svg width={screenParams.width} height={screenParams.height}>
-    //  {landerComp}
-    //</svg>;
-  return test;
+  const screen = 
+    <svg width={screenParams.width} height={screenParams.height}>
+      {genLanderComponent(landerPos)}
+    </svg>;
+  return screen
 }
 
-// eslint-disable-next-line
 function genLanderComponent(nodes) {
   const nodeComponents = nodes.map(
     (ele,ind) => <LanderNode key={ind} x={ele.x} y={ele.y} />
